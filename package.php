91eefa2f73048cbe5ee383066387e51d8cce0e3f
Stephan Schmidt <schst@php.net>
2004-04-17 09:02:50 +0000

    updated package file
    git-svn-id: http://svn.php.net/repository/pear/packages/XML_Beautifier/trunk@156386 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ package.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * script to automate the generation of the
+ * package.xml file.
+ *
+ * @author      Stephan Schmidt <schst@php-tools.net>
+ * @package     XML_Beautifier
+ * @subpackage  Tools
+ */
+
+/**
+ * uses PackageFileManager
+ */ 
+require_once 'PEAR/PackageFileManager.php';
+
+$version = '1.2.0';
+
+$notes = <<<EOT
+Added support for cdata sections (bug #1009)
+EOT;
+
+$description = <<<EOT
+XML_Beautifier will add indentation and linebreaks to you XML files, replace all
+entities, format your comments and makes your document easier to read. You can
+influence the way your document is beautified with several options.
+EOT;
+
+$package = new PEAR_PackageFileManager();
+
+$result = $package->setOptions(array(
+    'package'           => 'XML_Beautifier',
+    'summary'           => 'Class to format XML documents.',
+    'description'       => $description,
+    'version'           => $version,
+    'state'             => 'stable',
+    'license'           => 'PHP License',
+    'filelistgenerator' => 'cvs',
+    'ignore'            => array('package.php', 'package.xml'),
+    'notes'             => $notes,
+    'simpleoutput'      => false,
+    'baseinstalldir'    => 'XML',
+    'packagedirectory'  => './',
+    'dir_roles'         => array('docs' => 'doc',
+                                 'examples' => 'doc',
+                                 'tests' => 'test',
+                                 )
+    ));
+
+if (PEAR::isError($result)) {
+    echo $result->getMessage();
+    die();
+}
+
+$package->addMaintainer('schst', 'lead', 'Stephan Schmidt', 'schst@php-tools.net');
+
+$package->addDependency('XML_Parser', '', 'has', 'pkg', false);
+$package->addDependency('XML_Util', '0.5', 'ge', 'pkg', false);
+$package->addDependency('php', '4.2.0', 'ge', 'php', false);
+
+if (isset($_GET['make']) || (isset($_SERVER['argv'][1]) && $_SERVER['argv'][1] == 'make')) {
+    $result = $package->writePackageFile();
+} else {
+    $result = $package->debugPackageFile();
+}
+
+if (PEAR::isError($result)) {
+    echo $result->getMessage();
+    die();
+}
+?>
